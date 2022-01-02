e310576e842c431467ed368290a54de265e1ef9b
Nir Geron <75199660+GeroNir@users.noreply.github.com>
2022-01-02 16:41:18 +0200

    Update Algo.c

--- Algo.c
+++ Algo.c
@@ -1,7 +1,10 @@
 #define inf 100000000
 
 #include <malloc.h>
+#include <limits.h>
 #include "graph.h"
+
+int tsp = INT_MAX;
 
 int shortestPath(pnode *head, int src, int dest) {
     int dist[(*head)->size];
@@ -41,4 +44,86 @@ int shortestPath(pnode *head, int src, int dest) {
     free(queue);
     free(queue->array);
     return dist[dest];
+}
+
+//int tsp (pnode *head, int listofnode [], int size) {
+//    int dist[size][size];
+//    for(int i=0; i< size; i++){                         //build a matrix of dijestra
+//        for(int j=0; j<size; j++){
+//            if(i != j){
+//                dist[i][j]= shortestPath(head, listofnode[i], listofnode[j]);
+//            }else{
+//                dist[i][j]=inf;
+//            }
+//        }
+//    }
+//    int shortest = inf, sumofweight=0 ,min = inf, count=0;
+//    int tempshortestarray [size];
+//    int shortestarray [size];
+//    for(int i=0; i<(size); i++){
+//        for(int j=0; j<(size); j++){
+//                if(dist[i][j] < min){
+//                    min = dist[i][j];
+//                    tempshortestarray[i] = j;
+//                    sumofweight = sumofweight+dist[i][j];
+//                }
+//        }
+//        for(int j=0; j<(size); j++){
+//            if(tempshortestarray[i] != 0 || tempshortestarray[i] != inf) {
+//                count++;
+//            }
+//        }
+//        min=inf;
+//        if(shortest>sumofweight && count == size) {
+//            shortest = sumofweight;
+//            for(int j=0; j<(size); j++){
+//                shortestarray[j] = tempshortestarray[j];
+//                tempshortestarray[i] = 0;
+//            }
+//        }
+//        for(int j=0; j<(size); j++){
+//            tempshortestarray[i] = 0;
+//        }
+//    }
+//    int *p = shortestarray;
+//    return p;
+//}
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(pnode *head, int *a, int start, int end) {
+    if (start == end){
+        int sum = 0;
+        int j=1;
+        for(int i=0; i<end-1; i++){
+            if(j<=end){
+                sum += shortestPath(head, i, j);
+                j++;
+            }
+        }
+        if(sum<tsp){
+            tsp = sum;
+        }
+    }
+
+    else{
+        for(int i = start; i <= end; i++){
+            swap((a+start), (a+i));
+            permute(head, a, start+1, end);
+            swap((a+start), (a+i)); //backtrack
+        }
+    }
+}
+
+void TSP(pnode *head) {
+    int len = 3;
+    pnode *temp = head;
+    int arr[3] = {2, 1, 3};
+    tsp = INT_MAX;
+    permute(head, arr, 0, len-1);
+    printf("TSP shortest path: %d\n", tsp);
 }
